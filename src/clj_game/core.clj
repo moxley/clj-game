1d83b0f179ff6f950158c45d7d4845ae9068a4d3
Moxley Stratton <moxley@theclymb.com>
2013-01-19 13:57:03 -0800

    Collision of ball and bat

--- src/clj_game/core.clj
+++ src/clj_game/core.clj
@@ -6,6 +6,22 @@
 
 (def WIDTH 640)
 (def HEIGHT 480)
+(def X_SPEED 0.15)
+
+(def ball (atom {:x (- (/ WIDTH 2) (/ 10 2))
+                 :y (- (/ HEIGHT 2) (/ 10 2))
+                 :width 10
+                 :height 10
+                 :dx (* -1 X_SPEED)
+                 :dy 0.0}))
+
+(def bat (atom {:x 10
+                :y (- (/ HEIGHT 2) (/ 80 2))
+                :width 10
+                :height 80
+                :dx 0.0
+                :dy 0.0}))
+
 
 (defn setup-display []
   (let [mode (new DisplayMode WIDTH HEIGHT)]
@@ -35,20 +51,27 @@
      (+ (e :x) (e :width))
      (+ (e :y) (e :height)))))
 
-(defn set-up-entities []
-  ;ball = new Ball(WIDTH / 2 - 10 / 2, HEIGHT / 2 - 10 / 2, 10, 10);
-  (def ball (atom {:x (- (/ WIDTH 2) (/ 10 2))
-                   :y (- (/ HEIGHT 2) (/ 10 2))
-                   :width 10
-                   :height 10
-                   :dx -0.1}))
+(defn update-entity [entity delta]
+  (let [e @entity
+        x (+ (e :x) (* delta (e :dx)))
+        y (+ (e :y) (* delta (e :dy)))]
+    (swap! entity conj [:x x] [:y y])))
 
-  ;bat = new Bat(10, HEIGHT / 2 - 80 / 2, 10, 80);
-  (def bat (atom {:x 10
-                  :y (- (/ HEIGHT 2) (/ 80 2))
-                  :width 10
-                  :height 80
-                  :dx 0})))
+(defn flip-delta [entity]
+  (swap! entity conj [:dx (* -1 (@entity :dx))]))
+
+(defn collides? [e1 e2]
+  (and
+   (<= (e1 :x) (+ (e2 :x) (e2 :width)))
+   (>= (e1 :x) (e2 :x))
+   (>= (e1 :y) (e2 :y))
+   (<= (e1 :y) (+ (e2 :y) (e2 :height)))))
+
+(defn do-logic [delta]
+  (update-entity ball delta)
+  ;;bat.update(delta)
+  (if (collides? @ball @bat)
+    (flip-delta ball)))
 
 (defn render []
   (GL11/glClear GL11/GL_COLOR_BUFFER_BIT)
@@ -66,13 +89,12 @@
       (quit)
       (do 
         ; do game stuff
-        (println "delta: " delta)
         (render)
+        (do-logic delta)
         (update-display)
         (recur loop-time))))))
 
 (defn -main[]
-  (set-up-entities)
   (setup-display)
   (setup-opengl)
   (run-loop))
