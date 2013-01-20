c17f84ee5bbcac832f3b688118c58d083bfbdf2e
Moxley Stratton <moxley@theclymb.com>
2013-01-20 11:17:21 -0800

    rewrite border collision for flexibility and accuracy

--- src/clj_game/core.clj
+++ src/clj_game/core.clj
@@ -12,13 +12,30 @@
   (Display/destroy)
   (System/exit 0))
 
+(defn lose-point []
+  ;; Pause game, display message
+  (reset-ball ball)
+  (println "Lost one point"))
+
 (defn do-logic [delta]
   (update-position ball delta)
   (update-position paddle delta)
-  ;;paddle.update(delta)
-  (cond (collides? @ball @paddle) (flip-delta ball :dx)
-        (not (collides-x? @ball border)) (flip-delta ball :dx)
-        (not (collides-y? @ball border)) (flip-delta ball :dy)))
+  (if (collides? @ball @paddle) ;; Collides with paddle
+    (flip-delta ball :dx)
+
+    (let [[exit-x exit-y] (ball-exits-border-at @ball border)]
+      (cond
+       ;; Goes past paddle (at left wall); point lost
+       (< exit-x 0)
+       (lose-point)
+
+       ;; Hits right wall
+       (> exit-x 0)
+       (flip-delta ball :dx)
+
+       ;; Hits horizontal walls
+       (not (zero? exit-x))
+       (flip-delta ball :dy)))))
 
 (defn render []
   (GL11/glClear GL11/GL_COLOR_BUFFER_BIT)
--- src/clj_game/entities.clj
+++ src/clj_game/entities.clj
@@ -7,11 +7,15 @@
 ;;  Main game entities
 (def border {:x 0 :y 0 :width WIDTH :height HEIGHT})
 
-(def ball (atom {:x (- (/ WIDTH 2) (/ 10 2))
-                 :y (- (/ HEIGHT 2) (/ 10 2))
+(def BALL-DEFAULT-X (- (/ WIDTH 2) (/ 10 2)))
+(def BALL-DEFAULT-Y (- (/ HEIGHT 2) (/ 10 2)))
+(def BALL-DEFAULT-DX (* -1 X-SPEED))
+
+(def ball (atom {:x BALL-DEFAULT-X
+                 :y BALL-DEFAULT-Y
                  :width 10
                  :height 10
-                 :dx (* -1 X-SPEED)
+                 :dx BALL-DEFAULT-DX
                  :dy 0.0}))
 
 (def paddle (atom {:x 10
@@ -41,19 +45,46 @@
 (defn update-entity [entity values]
   (swap! entity into values))
 
-(defn collides-x? [e1 e2]
+(defn reset-ball [ball]
+  (update-entity ball {:x BALL-DEFAULT-X
+                       :y BALL-DEFAULT-Y
+                       :dx BALL-DEFAULT-DX}))
+
+(defn collides? [e1 e2]
   (and
    (<= (e1 :x) (+ (e2 :x) (e2 :width)))
-   (>= (e1 :x) (e2 :x))))
-
-(defn collides-y? [e1 e2]
-  (and
+   (>= (e1 :x) (e2 :x))
    (>= (e1 :y) (e2 :y))
    (<= (e1 :y) (+ (e2 :y) (e2 :height)))))
 
-(defn collides? [e1 e2]
-  (and (collides-x? e1 e2)
-       (collides-y? e1 e2)))
+;; Returns a 2-dimentional vector representing
+;; which walls the ball is exiting
+;; [-1 0] means the ball is exiting the left wall
+;; [1 0] means the ball is exiting the right wall
+;; [0 -1] top wall
+;; [0 1] bottom wall
+(defn ball-exits-border-at [ball border]
+  (let [b ball
+        b-x1 (b :x)
+        b-x2 (+ b-x1 (b :width))
+        b-y1 (b :y)
+        b-y2 (+ b-y1 (b :height))
+
+        r border
+        r-x1 (r :x)
+        r-x2 (+ r-x1 (r :width))
+        r-y1 (r :y)
+        r-y2 (+ r-y1 (r :height))
+
+        wall-vx (cond
+                 (<= b-x1 r-x1) -1
+                 (>= b-x2 r-x2) 1
+                 :else 0)
+        wall-vy (cond
+                 (<= b-y1 r-y1) -1
+                 (>= b-y2 r-y2) 1
+                 :else 0)]
+    [wall-vx wall-vy]))
 
 (defn flip-delta [entity key]
   (swap! entity conj [key (* -1 (@entity key))]))
