700c6f450df84f80bb6a34d65f74308eaf414799
Christopher Kuttruff <kuttruff@gmail.com>
2013-01-23 20:59:03 -0800

    update the url in the lein project file

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
 (defproject clj-game "0.1.0-SNAPSHOT"
-  :description "A game... in clojure!"
-  :url "http://example.com/FIXME"
+  :description "A pong game... in clojure!"
+  :url "https://github.com/moxley/clj-game"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.lwjgl/lwjgl "2.7.1"]
                  [org.lwjgl/lwjgl-util "2.7.1"]
                  [org.lwjgl/lwjgl-native-platform "2.7.1"]]
-
   :main clj-game.core)
