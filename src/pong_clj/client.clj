f340b6ca1bd39677047edf10565ca5c731fdb868
Moxley Stratton <moxley@theclymb.com>
2013-02-05 22:28:47 -0800

    refactor the client network setup

--- src/pong_clj/client.clj
+++ src/pong_clj/client.clj
@@ -24,31 +24,28 @@
 (def inputs (atom {}))
 
 (defn iteration [remote]
+  "Client-side network loop iteration"
   (let [new-inputs {:key-up (input/key-up?) :key-down (input/key-down?)}]
     (send-input-diff @inputs new-inputs remote)
     (swap! inputs into new-inputs))
   (pause))
 
-(defn client-loop [conn]
-  (let [clientSocket (DatagramSocket.)
-        addr (InetAddress/getByName (:host conn))
-        _ (println "IPAddress:" addr)
-        remote (into conn {:addr addr :socket clientSocket})]
-    (while (:running? @entities/game) (iteration remote))
-    (println "Disconnecting")
-    (.close clientSocket)))
-
-(defn connect [server]
-  (doto (Thread. #(client-loop server)) (.start)))
+(defn client-loop [remote]
+  "Run the client side network loop"
+  (while (:running? @entities/game) (iteration remote))
+  (println "Disconnecting")
+  (.close (:socket remote)))
 
 (defn connect-to-server []
   (println "Connecting to server...")
   (try
-    (connect {:name "localhost" :port net/UDP_PORT})
+    (let [clientSocket (DatagramSocket.)
+          server {:host "localhost" :port net/UDP_PORT}
+          addr (InetAddress/getByName (:host server))
+          _ (println "IPAddress:" addr)
+          remote (into server {:addr addr :socket clientSocket})]
+      (doto (Thread. #(client-loop remote)) (.start)))
     (catch ConnectException e (println "Failed to connect to server"))))
-
-(defn setup-network []
-  (when (@entities/game :network?) (connect-to-server)))
 
 (defn quit []
   (swap! entities/game into {:running? false})
@@ -74,6 +71,6 @@
 
 (defn main [& rest]
   (display/setup)
-  (setup-network)
+  (when (@entities/game :network?) (connect-to-server))
   (swap! entities/game into {:running? true})
   (run-loop))
