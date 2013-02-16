d8bfa72c2ca55d0c5200ee57020dafcac3c9c779
Moxley Stratton <moxley@theclymb.com>
2013-02-16 09:48:31 -0800

    Replace implementation of key-up? and key-down?
    Use event queue model implementation

--- src/pong_clj/input.clj
+++ src/pong_clj/input.clj
@@ -8,7 +8,7 @@
 (defn parse-lwjgl-event [e now]
   {:key (:event-key e)
    :name (:key-name e)
-   :state (if (true? (:event-key-state e)) :up :down)
+   :state (if (true? (:event-key-state e)) :down :up)
    :time (/ (e :event-nanoseconds) 1000000)
    :record-time now})
 
@@ -29,12 +29,27 @@
     (swap! client-event-queue into events)
     (swap! server-event-queue into events)))
 
-(defn key-up? []  (Keyboard/isKeyDown Keyboard/KEY_UP))
-(defn key-down? [] (Keyboard/isKeyDown Keyboard/KEY_DOWN))
+(defn key-pressed?
+  ([key] (key-pressed? key @client-event-queue))
+  ([key queue]
+    (loop [down? false
+           remaining-events queue]
+      (let [e (first remaining-events)
+            r (rest remaining-events)]
+        (if (not e)
+          down?
+          (if (= (:name e) key)
+            (recur (= (:state e) :down) r)
+            (recur down? r)))))))
+
+(defn key-up?
+  ([] (key-up? @client-event-queue))
+  ([queue] (key-pressed? "UP" queue)))
+
+(defn key-down?
+  ([] (key-down? @client-event-queue))
+  ([queue] (key-pressed? "DOWN" queue)))
 
 (defn handle-input [delta]
-  ;;(save-keyboard-events delta)
-  ;;(println "client-event-queue:" @client-event-queue)
-  ;;(swap! inputs into {:key-up false :key-down false})
-  ;;(println "save-keyboard-events:" (save-keyboard-events delta))
+  (save-keyboard-events delta)
   (swap! inputs into {:key-up (key-up?) :key-down (key-down?)}))
---
+++ test/pong_clj/input_test.clj
@@ -0,0 +1,28 @@
+(ns pong-clj.input-test
+  (:use clojure.test
+        pong-clj.input))
+
+(defn select [f s]
+  (keep #(if (f %) % nil) s))
+
+(defn down-events [queue]
+  (select #(= "DOWN" (:name %)) queue))
+
+(deftest a-test
+  (let [event-queue [{:key 205, :name "RIGHT", :state :down, :time 1361001074800, :record-time 1361001074808}
+                     {:key 205, :name "RIGHT", :state :up, :time 1361001074808, :record-time 1361001074824}
+                     {:key 205, :name "RIGHT", :state :down, :time 1361001074901, :record-time 1361001074908}
+                     {:key 205, :name "RIGHT", :state :up, :time 1361001075057, :record-time 1361001075073}
+                     {:key 208, :name "DOWN", :state :down, :time 1361001075085, :record-time 1361001075092}
+                     {:key 205, :name "RIGHT", :state :down, :time 1361001075131, :record-time 1361001075157}
+                     {:key 205, :name "RIGHT", :state :up, :time 1361001075357, :record-time 1361001075374}
+                     {:key 208, :name "DOWN", :state :up, :time 1361001075523, :record-time 1361001075540}
+                     {:key 200, :name "UP", :state :down, :time 1361001075793, :record-time 1361001076542}
+                     ;;{:key 200, :name "UP", :state :up, :time 1361001076125, :record-time 1361001076542}
+                     {:key 219, :name "LWIN", :state :down, :time 1361001076809, :record-time 1361001076828}]]
+    (testing "(key-down?) should be true"
+      (let [state (key-down? event-queue)]
+        (is (false? state))))
+    (testing "(key-up?) should be false"
+      (let [state (key-up? event-queue)]
+        (is (true? state))))))
